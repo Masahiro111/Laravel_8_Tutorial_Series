0cdf6e884adf352f91dfbb347710900863a5a6e8
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-16 19:24:01 +0900

    finished part 13

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PostController extends Controller
+{
+
+    public function getAllPost()
+    {
+        $posts = DB::table('posts')->get();
+        return view('posts', compact('posts'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\FluentController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\UserController;
@@ -58,3 +59,6 @@ Route::get('/session/set', [SessionController::class, 'storeSessionData'])
 
 Route::get('/session/remove', [SessionController::class, 'deleteSessionData'])
     ->name('session.delete');
+
+Route::get('/posts', [PostController::class, 'getAllPost'])
+    ->name('post.getallpost');
