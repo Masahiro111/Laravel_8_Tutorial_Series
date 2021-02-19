81bb448d40c02d0d01dc7e90160db799138b032f
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-19 19:17:12 +0900

    finished part 20

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('title','Home')
+
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <h1>Home Page Content</h1>
+    </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -101,3 +101,15 @@ Route::get('/all-posts', [PostController::class, 'getAllPostUsingModel'])
 Route::get('/test', function () {
     return view('test');
 });
+
+Route::get('/home', function () {
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
