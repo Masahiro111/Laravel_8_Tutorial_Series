21f4d6d7a9fffb8de9d0add0e163fbfe03c6d887
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-20 23:40:02 +0900

    finished part 23

---
+++ resources/lang/ja/messasge.php
@@ -0,0 +1,5 @@
+<?php
+return [
+    "welcome" => "ようこそ、Laravel 8へ",
+    "language" => "日本語"
+];
--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/locale/{locale}', function ($locale) {
+//     App::setLocale($locale);
+//     return view('welcome');
+// });
+
 Route::get('/', [ProductController::class, 'index'])
     ->name('product.index');
 
