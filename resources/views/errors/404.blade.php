92562cced7c70495bb2b06baf41eda5fcf23a452
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-21 02:16:29 +0900

    finished part 25

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Page Not Found</title>
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+</head>
+
+<body>
+    <section style="padding-top: 100px;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8 offset-md-2 text-center">
+                    <h1>404 Not Found</h1>
+                    <h2>Sorry , Page is not found</h2>
+                    <a href="/" class="btn btn-primary" style="margin-top: 3rem;">To Home</a>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/popper.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+</body>
+
+</html>
