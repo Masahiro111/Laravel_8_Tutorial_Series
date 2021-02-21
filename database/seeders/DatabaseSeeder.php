210be3a4288735c92aa60f30587b59a8dc395ee9
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-21 16:38:01 +0900

    finished part 28

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+
         $faker = Faker::create();
+
         foreach (range(1, 100) as $index) {
-            DB::table('users')->insert([
+
+            DB::table('students')->insert([
                 'name' => $faker->name,
                 'email' => $faker->email,
-                'password' => bcrypt('secret')
+                'phone' => $faker->phoneNumber
             ]);
         }
+        // $faker = Faker::create();
+        // foreach (range(1, 100) as $index) {
+        //     DB::table('users')->insert([
+        //         'name' => $faker->name,
+        //         'email' => $faker->email,
+        //         'password' => bcrypt('secret')
+        //     ]);
+        // }
         // \App\Models\User::factory(10)->create();
         // $this->call([
         //     PostTableSeeder::class,
--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\PaginationController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\SessionController;
+use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UploadController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
@@ -134,3 +135,6 @@ Route::post('/upload', [UploadController::class, 'uploadFile'])
 Route::get('/payment', function () {
     return Payment::Process();
 });
+
+
+Route::get('/students', [StudentController::class, 'fetchStudents']);
