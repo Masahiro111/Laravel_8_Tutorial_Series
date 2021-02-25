532939948061ba8893e9f33e2c2f3dcc26b7fca3
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-25 17:00:47 +0900

    Revert "add stub"
    This reverts commit 0c0b1f3f4a77061b69e8d90db39665aba1d8c07d.

--- config/app.php
+++ config/app.php
@@ -162,7 +162,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         App\Providers\PaymentServiceProvider::class,
-        \Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -229,7 +228,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Payment' => App\PaymentGateway\PaymentFacade::class,
-        'Excel' => \Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
