3e32951092ebda936b9081f2ca2bfc35a7fb3526
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-25 17:01:55 +0900

    Revert "finished part 30"
    This reverts commit b11695bf52dec1ecf9e36518b83d3a4b61a8e098.

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Phone;
-use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -11,26 +9,5 @@ class UserController extends Controller
     public function index(Request $request)
     {
         return $request->path();
-    }
-
-    public function insertRecord()
-    {
-        $phone = new Phone();
-        $phone->phone = "1234567890";
-
-        $user = new User();
-        $user->name = "john";
-        $user->email = "user@example.com";
-        $user->password = encrypt('password');
-        $user->save();
-        $user->phone()->save($phone);
-
-        return "record has been created successfully";
-    }
-
-    public function fetchPhoneByUser($id)
-    {
-        $phone = User::find($id)->phone;
-        return $phone;
     }
 }
