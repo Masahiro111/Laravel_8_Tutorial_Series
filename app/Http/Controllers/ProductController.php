dbc5e5113898e35c9963819abfad4d5984a736ce
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-02-26 19:19:54 +0900

    finished part 45

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,13 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
-    public function index()
+    public function addProducts()
     {
-        $fruits = array('Mango', 'Orange', 'Banana', 'Apple', 'Pineapple');
+        $products = [
+            ["name" => "Phone"],
+            ["name" => "Tablet"],
+            ["name" => "Laptops"],
+            ["name" => "Watch"],
+            ["name" => "Television"],
+            ["name" => "Freeze"],
+        ];
 
-        return view('welcome', compact('fruits'));
+        Product::insert($products);
+        return "Product has been inserted successfully";
+    }
+
+    public function search()
+    {
+        return view('search');
+    }
+
+    public function autocomplete(Request $request)
+    {
+        $datas = Product::select("name")
+            ->where("name", "LIKE", "%{$request->terms}%")
+            ->get();
+        return response()->json($datas);
     }
 }
